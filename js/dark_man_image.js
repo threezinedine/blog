ccc3dfeaa88a5fa087b93eb92a2643737dcd9c35
unknown <threezinedine@gmail.com>
2022-08-11 11:14:26 +0700

    update image

--- js/dark_man_image.js
+++ js/dark_man_image.js
@@ -1,11 +1,12 @@
 export default {
     data() {
         return {
-            imageSrc: "../images/dark-man.jpg"
+            imageSrc: "../images/dark-man.jpg",
+            imageSrc2: "../blog/images/dark-man.jpg",
         }
     },
     template: `
-        <div class="background-image" :style="'background-image: url(' + imageSrc + ')'">
+        <div class="background-image" :style="'background-image: url(' + imageSrc + '), url('+ imageSrc2 +')'">
         </div>
     `
 }
